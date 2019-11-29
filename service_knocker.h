23d10743023404deeb69c77b5a4bc9fc54b83bd7
NakagamiYuta <guguru0014@gmail.com>
2019-11-29 23:38:25 +0900

    [add] SUCCESS, FAILED40x, FAILED50xマクロ追加

--- service_knocker.h
+++ service_knocker.h
@@ -4,6 +4,10 @@
 #include <cstdint>
 #include <string>
 
+#define SUCCESS(x)   (int)(x)/100==2
+#define FAILED40x(x) (int)(x)/100==4
+#define FAILED50x(x) (int)(x)/100==5
+
 namespace yn0014 {
 
 class ServiceKnocker{
