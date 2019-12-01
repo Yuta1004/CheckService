d596fd1235774b5e90b38dd2aa32d3f1941ea40d
NakagamiYuta <guguru0014@gmail.com>
2019-12-01 23:27:38 +0900

    [change] TCPConnectorのテストを変更

--- test/test_tcp_connector.cpp
+++ test/test_tcp_connector.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 #include "../net/network_connector.h"
 
 using std::cout;
@@ -9,11 +11,6 @@ int main()
     // 172.217.26.14  => google.com
     // 153.120.168.14 => www.yuge.ac.jp
     yn0014::net::TCPConnector tcpA("153.120.168.14");
-
-    if(tcpA.sendMsg("GET / HTTP/1.0\r\n\r\n"))
-        cout << "Success" << endl;
-    else
-        cout << "Failed" << endl;
-
+    tcpA.sendMsg("GET / HTTP/1.0\r\n\r\n");
     cout << tcpA.getRecv() << endl;
 }
