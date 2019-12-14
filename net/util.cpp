9a67bc93ebfb2ce03bcbb56a76e7dce91817a7eb
NakagamiYuta <guguru0014@gmail.com>
2019-12-14 13:49:09 +0900

    [add] checkIPv4Formatをnet/utilに移動

---
+++ net/util.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include "mystring/mystring.h"
+#include "net/util.h"
+
+
+bool yn0014::net::util::checkIPv4Format(std::string strIP)
+{
+    std::vector<std::string> ipElems = yn0014::mystring::split(strIP, ".");
+    if(ipElems.size() != 4)
+        return false;
+
+    for(int32_t idx = 0; idx < 4; ++ idx) {
+        int32_t num = std::atoi(ipElems[idx].c_str());
+        if(num < 0 || 255 < num)
+            return false;
+    }
+    return true;
+}
---
+++ net/util.h
@@ -0,0 +1,18 @@
+#ifndef NET_UTIL_H
+#define NET_UTIL_HB
+
+#include <string>
+
+namespace yn0014 {
+
+    namespace net {
+
+        namespace util {
+            bool checkIPv4Format(std::string strIP);
+        }
+
+    }
+
+}
+
+#endif
