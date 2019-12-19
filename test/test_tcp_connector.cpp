8f414cc096c4c50638eefb5d23b29a70180ac49e
NakagamiYuta <guguru0014@gmail.com>
2019-12-19 23:23:23 +0900

    [change] テストに使用するサーバを変更

--- test/test_tcp_connector.cpp
+++ test/test_tcp_connector.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <cstddef>
 #include <cstring>
 #include "../net/network_connector.h"
 
@@ -10,8 +11,7 @@ int main()
 {
     // 172.217.26.14  => google.com
     // 153.120.168.14 => www.yuge.ac.jp
-    /* yn0014::net::TCPConnector tcpA("153.120.168.14"); */
-    yn0014::net::TCPConnector tcpA("163.44.172.209");
+    yn0014::net::TCPConnector tcpA("153.120.168.14");
     tcpA.sendMsg("GET / HTTP/1.0\nUser-Agent:yn0014-TCPConnector\n\n");
     tcpA.getRecv();
 }
