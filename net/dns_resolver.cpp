96262080bca6475d5e3da43f3a4bda9457875ddd
NakagamiYuta <guguru0014@gmail.com>
2019-12-20 23:08:01 +0900

    [add] makeDNSReqMsg実装
    未検証、多分大丈夫だとは思う

--- net/dns_resolver.cpp
+++ net/dns_resolver.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <vector>
 #include <string>
 #include "dns_resolver.h"
@@ -28,15 +29,21 @@ yn0014::net::DNSResolver::DNSResolver(std::string masterServerIP)
     }
 }
 
-void yn0014::net::DNSResolver::makeDNSReqMsg(std::string hostURL)
+uint8_t *yn0014::net::DNSResolver::makeDNSReqMsg(std::string hostURL)
 {
-    /* DNS Query Header */
+
+    std::vector<std::string> labels = yn0014::mystring::split(hostURL, ".");
+    size_t hd_size = 6 * sizeof(uint16_t), qb_size = 0;
+    uint16_t header[6] = {0};
+    uint8_t *qsecBody = (uint8_t*)calloc(labels.size()*15+5, sizeof(uint8_t));
+
+    // ** DNS Query Header **
     // - ID         16  C   (任意)
     // - QR         1   C   (問合せの場合は0)
     // - OPCODE     4   C   (通常クエリは0)
     // - AA         1   S   (管理権限のある応答の場合1)
     // - TC         1   S   (応答が切り詰められている場合1)
-    // - RD         1   C   (名前解決要求、権威DNSサーバへのリクエストは0)
+    // - RD         1   C   (名前解決要求、フルサービスリゾルバへのリクエストは1)
     // - RA         1   S   (名前解決可能な場合0)
     // - Z          1   -   (予約bit, 0にする)
     // - AD         1   S   (DNSECに成功した場合1)
@@ -46,4 +53,26 @@ void yn0014::net::DNSResolver::makeDNSReqMsg(std::string hostURL)
     // - ANCOUNT    16  S   (Answerセクションの数)
     // - NSCOUNT    16  S   (Authorityセクションの数)
     // - ARCOUNT    16  S   (Additionalセクションの数)
+    header[1] |= RD;
+
+    // ** DNS Qestion Section **
+    // - size   1       (ラベルのサイズ)
+    // - label  可変長  (ラベル, ASCII)
+    for(auto part : labels) {
+        qsecBody[qb_size++] = part.size();
+        for(char c : part) {
+            qsecBody[qb_size++] = c;
+        }
+    }
+    qsecBody[qb_size++] = 0;
+    qsecBody[qb_size++] = 1;
+    qsecBody[qb_size++] = 0;
+    qsecBody[qb_size++] = 0xff;
+
+    // 組み立て
+    uint8_t *msg = (uint8_t*)malloc(hd_size+qb_size);
+    memcpy(msg, header, hd_size);
+    memcpy(msg+hd_size, qsecBody, qb_size);
+    free(qsecBody);
+    return msg;
 }
--- net/dns_resolver.h
+++ net/dns_resolver.h
@@ -4,6 +4,17 @@
 #include <vector>
 #include <string>
 
+#define QR      (0b1000000000000000)
+#define OPCODE  (0b0111100000000000)
+#define AA      (0b0000010000000000)
+#define TC      (0b0000001000000000)
+#define RD      (0b0000000100000000)
+#define RA      (0b0000000010000000)
+#define Z       (0b0000000001000000)
+#define AD      (0b0000000000100000)
+#define CD      (0b0000000000010000)
+#define RCODE   (0b0000000000001111)
+
 namespace yn0014 {
 
     namespace net {
@@ -17,7 +28,7 @@ namespace yn0014 {
 
         private:
             std::string masterServerIP;
-            void makeDNSReqMsg(std::string hostURL);
+            unsigned char *makeDNSReqMsg(std::string hostURL);
 
         };
 
