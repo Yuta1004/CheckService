6aa0a1be9dc6456df5cf04d2659d53dcb6cafe92
NakagamiYuta <guguru0014@gmail.com>
2019-12-23 23:35:24 +0900

    [update] test_service_knocker

--- test/test_service_knocker.cpp
+++ test/test_service_knocker.cpp
@@ -1,11 +1,31 @@
+#include <cstdint>
 #include <iostream>
 #include "../service_knocker.hpp"
 
 using std::cout;
 using std::endl;
 
+std::string getStatusMsg(int32_t stcode)
+{
+    if(SUCCESS(stcode))
+        return "200 OK";
+    if(FAILED40x(stcode))
+        return "40x Client Error";
+    if(FAILED50x(stcode))
+        return "50x Server Error";
+    return "Unknown Error;"
+}
+
 int main()
 {
     yn0014::ServiceKnocker sknocker("http://www.yuge.ac.jp/subject/info");
     sknocker.knock();
+
+    std::string result = getStatusMsg(sknocker.getStatus());
+    std::vector<std::string> resp = sknocker.getResp();
+
+    cout << "Status: " << result << endl;
+    cout << "Response:" << endl;
+    for(auto line : resp)
+        cout << line << endl;
 }
