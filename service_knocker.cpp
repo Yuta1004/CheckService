30f5714e765c79ff6dbc27b35ffe0426736f8c07
NakagamiYuta <guguru0014@gmail.com>
2019-12-24 23:43:54 +0900

    [add] ServiceKnocker::getStatus実装

--- service_knocker.cpp
+++ service_knocker.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <cstdlib>
 #include "service_knocker.hpp"
 #include "net/tcp_connector.hpp"
 #include "net/dns_resolver.hpp"
@@ -7,6 +9,7 @@
 #include "mystring/mystring.hpp"
 
 using std::cout;
+using std::cerr;
 using std::endl;
 
 yn0014::ServiceKnocker::ServiceKnocker(std::string serviceURL)
@@ -33,6 +36,13 @@ void yn0014::ServiceKnocker::knock()
 
 int32_t yn0014::ServiceKnocker::getStatus()
 {
+    if(resp.size() == 0) {
+        cerr << "Cannot parse status" << endl;
+        return -1;
+    }
+
+    std::string statusStr = yn0014::mystring::split(resp[0], " ")[1];   // [HTTP/1.1, 200, OK]
+    result = (int32_t)strtol(statusStr.c_str(), nullptr, 10);
     return result;
 }
 
