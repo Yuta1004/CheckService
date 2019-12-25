6b27c6fd784d64534907105c7cff7dd6fc65be01
NakagamiYuta <guguru0014@gmail.com>
2019-12-25 15:26:03 +0900

    [update] test_tcp_connector

--- test/test_tcp_connector.cpp
+++ test/test_tcp_connector.cpp
@@ -11,8 +11,19 @@ int main()
 {
     // 172.217.26.14  => google.com
     // 153.120.168.14 => www.yuge.ac.jp
+    // 52.69.186.44 => github.com
+    uint8_t *resp;
+
     yn0014::net::TCPConnector tcpA("153.120.168.14");
     tcpA.sendMsg("GET / HTTP/1.0\r\n\r\n");
-    uint8_t *resp = tcpA.getRecv();
+    resp = tcpA.getRecv();
+    cout << (char*)resp << endl << endl;
+    free(resp);
+
+    yn0014::net::TCPConnector tcpB("52.69.186.44", 443);
+    tcpB.startSSL();
+    tcpB.sendMsg("GET /Yuta1004 HTTP/1.0\r\nHost: github.com\r\n\r\n");
+    resp = tcpB.getRecv();
+    cout << (char*)resp << endl << endl;
     free(resp);
 }
