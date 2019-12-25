1dad46bfdab2266a080226eeea4a3b04bbca4594
NakagamiYuta <guguru0014@gmail.com>
2019-12-25 16:30:19 +0900

    [add] main実装OK

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <fstream>
 #include <iostream>
+#include "service_knocker.hpp"
+
+#define convert_bool_to_ok(cond) ((cond) ? "OK" : "Failed")
 
 using std::cout;
 using std::cerr;
 using std::endl;
+
+bool knock(std::string url)
+{
+    yn0014::ServiceKnocker knocker(url);
+    knocker.knock();
+    return SUCCESS(knocker.getStatus());
+}
 
 int main(int argc, char* argv[])
 {
@@ -17,5 +27,12 @@ int main(int argc, char* argv[])
     std::ifstream ifs(argv[1]);
     if(ifs.fail()) {
         cerr << "Failed to open file: [" << argv[1] << "]" << endl;
+    }
+
+    // 生存確認
+    std::string url;
+    while(getline(ifs, url)) {
+        bool result = knock(url);
+        cout << "- " << url << " : " << convert_bool_to_ok(result) << endl;
     }
 }
