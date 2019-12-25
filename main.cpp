4d14a087efacc8a8cf8cab2f47f045cb4c9a169a
NakagamiYuta <guguru0014@gmail.com>
2019-12-25 16:19:14 +0900

    [add] main.cppにファイル読み込み処理を書いた

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+#include <fstream>
 #include <iostream>
 
 using std::cout;
+using std::cerr;
 using std::endl;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World" << endl;
+    // 引数
+    if(argc != 2) {
+        cerr << "Usage : ./check_service <urlfile>" << endl;
+        exit(1);
+    }
+
+    // ifstream初期化
+    std::ifstream ifs(argv[1]);
+    if(ifs.fail()) {
+        cerr << "Failed to open file: [" << argv[1] << "]" << endl;
+    }
 }
