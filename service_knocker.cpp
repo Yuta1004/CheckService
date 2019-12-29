ab4f1d53b26f4b4050dc35a480ecfa2cb7e539ae
NakagamiYuta <guguru0014@gmail.com>
2019-12-29 18:08:17 +0900

    [add] service_knockerにcheck_conn_errorマクロを登録した

--- service_knocker.cpp
+++ service_knocker.cpp
@@ -8,12 +8,15 @@
 #include "net/util.hpp"
 #include "mystring/mystring.hpp"
 
+#define check_conn_error(result) { if(!(result)) return; }
+
 using std::cout;
 using std::cerr;
 using std::endl;
 
 yn0014::ServiceKnocker::ServiceKnocker(std::string serviceURL)
 {
+    cout << serviceURL << endl;
     result = 0;
     yn0014::net::DNSResolver resolver;
     url = new yn0014::net::util::URLParser(serviceURL);
@@ -31,19 +34,21 @@ void yn0014::ServiceKnocker::knock()
     yn0014::net::TCPConnector *conn;
     if(url->protocol == "https") {
         conn = new yn0014::net::TCPConnector(hostIP, 443);
-        conn->startConn();
-        conn->startSSL();
+        check_conn_error(conn->startConn());
+        check_conn_error(conn->startSSL());
     } else {
         conn = new yn0014::net::TCPConnector(hostIP);
-        conn->startConn();
+        check_conn_error(conn->startConn());
     }
 
     // 送信->レスポンス受け取り
-    conn->sendMsg(
-        yn0014::mystring::format(
-            "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n",
-            url->docpath.c_str(),
-            url->host.c_str()
+    check_conn_error(
+        conn->sendMsg(
+            yn0014::mystring::format(
+                "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n",
+                url->docpath.c_str(),
+                url->host.c_str()
+            )
         )
     );
     resp = yn0014::mystring::split((const char*)conn->getRecv(), "\n");
